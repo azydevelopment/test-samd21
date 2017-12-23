07226eec96dcda15085f143de989757f45c69262
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2017-12-23 02:22:11 -0800

    Modified max7219 project to create and use explicit clock object

--- test_samd21/max7219/program.h
+++ test_samd21/max7219/program.h
@@ -1,8 +1,8 @@
 #pragma once
 
+#include <azydev/embedded/bus/spi/maxim/max7219/transcoder.h>
 #include <azydev/embedded/logging/atmel/samd21/logger.h>
 #include <azydev/embedded/pins/atmel/samd21/pins.h>
-#include <azydev/embedded/bus/spi/maxim/max7219/transcoder.h>
 
 #include <stdint.h>
 
@@ -10,33 +10,34 @@
 
 template<typename TRANSFER_PRIMITIVE>
 class CSPIBus;
+
 class CSPIDevice;
+class CClock;
 
 class CProgram final
 {
-	public:
+public:
+    CProgram();
+    virtual ~CProgram();
+    void Main();
 
-	CProgram();
-	virtual ~CProgram();
-	void Main();
+private:
+    static const uint8_t SPI_BUS_ID;
+    static const uint8_t NUM_SPI_BUS_DEVICES;
+    static const uint8_t SPI_BUS_DEVICE_0_ID;
+    static const uint8_t SPI_BUS_DEVICE_0_SS_PIN;
+    static const uint8_t NUM_TEST_CASES;
 
-	private:
-	static const uint8_t SPI_BUS_ID;
-	static const uint8_t NUM_SPI_BUS_DEVICES;
-	static const uint8_t SPI_BUS_DEVICE_0_ID;
-	static const uint8_t SPI_BUS_DEVICE_0_SS_PIN;
-	static const uint8_t NUM_TEST_CASES;
+    // member variables
+    CLoggerAtmelSAMD21 m_logger;
+    CPinsAtmelSAMD21 m_pins;
+    CClock* m_spi_clock;
+    CSPIBus<uint16_t>* m_spi_bus;
+    CSPIDevice* m_spi_device;
+    CTranscoderMAX7219<uint16_t> m_transcoder_max7219;
+    double m_time;
 
-	// member variables
-	CLoggerAtmelSAMD21 m_logger;
-	CPinsAtmelSAMD21 m_pins;
-	CSPIBus<uint16_t>* m_spi_bus;
-	CSPIDevice* m_spi_device;
-	CTranscoderMAX7219<uint16_t> m_transcoder_max7219;
-	double m_time;
-
-	// methods
-	void OnInit();
-	void OnUpdate();
+    // methods
+    void OnInit();
+    void OnUpdate();
 };
-
