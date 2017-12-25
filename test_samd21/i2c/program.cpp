be218e5353bceaffb592bc05481b50c6ec94e5de
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2017-12-25 01:28:48 -0800

    Modified I2C test to use new explicit azydev clock object

--- test_samd21/i2c/program.cpp
+++ test_samd21/i2c/program.cpp
@@ -1,86 +1,102 @@
 #include "program.h"
 
-#include <cstring>
 #include <abdeveng/core/test/common/evaluator.h>
 #include <abdeveng/core/test/common/harness.h>
-#include <azydev/embedded/util/binary.h>
 #include <azydev/embedded/bus/i2c/atmel/samd21/bus.h>
+#include <azydev/embedded/clock/atmel/samd21/clock.h>
+#include <azydev/embedded/util/binary.h>
+#include <cstring>
 
 /* PUBLIC */
 
-CProgram::CProgram() :
-m_logger(CLoggerAtmelSAMD21()),
-m_pins(CPinsAtmelSAMD21()),
-m_i2c_bus(nullptr) {
+CProgram::CProgram()
+    : m_pins()
+    , m_i2c_clock(nullptr)
+    , m_i2c_bus(nullptr) {
 }
 
 CProgram::~CProgram() {
 }
 
 void CProgram::Main() {
-	OnInit();
+    OnInit();
 
-	while (1) {
-		OnUpdate();
-	}
+    while (1) {
+        OnUpdate();
+    }
 }
 
 void CProgram::OnInit() {
-	// init system
-	system_init();
-	m_logger.Init();
-	m_logger.SetEnabled(true);
+    // init system
+    system_init();
 
-	// create I2C bus
-	{
-		// setup pin config
-		CI2CBusAtmelSAMD21::PIN_CONFIG_DESC pinConfig = {};
-		{
-			// TODO IMPLEMENT
-			pinConfig.pad0 = PINMUX_PA08C_SERCOM0_PAD0;
-			pinConfig.pad1 = PINMUX_PA09C_SERCOM0_PAD1;
-			pinConfig.pad2 = PINMUX_UNUSED;
-			pinConfig.pad3 = PINMUX_UNUSED;
-		}
-		
-		CI2CBusAtmelSAMD21::DESC desc = {};
-		desc.id = 0;
-		desc.pin_config = pinConfig;
-		
-		// create I2C bus
-		// TODO: Remove dynamic memory allocation
-		m_i2c_bus = new CI2CBusAtmelSAMD21(desc, m_pins);
-	}
-	
-	// configure I2C bus
-	{
-		CI2CBusAtmelSAMD21::CONFIG_DESC busConfig = {};
-		{
-			busConfig.enable_run_in_standby = true;
-			busConfig.sda_hold_time = CI2CBusAtmelSAMD21::SDA_HOLD_TIME::NS_450;
-			busConfig.manager_baud = 0x23; // TODO HACK
-			busConfig.enable_smart_mode = true;
-			busConfig.transfer_speed = CI2CBusAtmelSAMD21::TRANSFER_SPEED::STANDARD;
-		}
-		
-		// set config
-		m_i2c_bus->SetConfig(busConfig);
-		
-		// set role
-		// TODO HACK: This I2C bus only supports ROLE::MASTER
-		m_i2c_bus->SetRole(CI2CEntity::ROLE::MANAGER);
-		
-		// enable I2C
-		m_i2c_bus->SetEnabled(true);
-	}
-	
+    // init I2C clock
+    {
+        CClockAtmelSAMD21::DESC desc = {};
+        desc.id                      = SERCOM2_GCLK_ID_CORE;
+        desc.bus                     = CClockAtmelSAMD21::BUS::APBC;
+        desc.pm_index                = PM_APBCMASK_SERCOM2_Pos;
+
+        m_i2c_clock = new CClockAtmelSAMD21(desc);
+
+        CClockAtmelSAMD21::CONFIG_DESC config = {};
+        config.clock_source_generator         = GCLK_GENERATOR_0;
+
+        m_i2c_clock->SetConfig(config);
+
+        // leave clock enabled
+        m_i2c_clock->SetEnabled(true);
+    }
+
+    // create I2C bus
+    {
+        // setup pin config
+        CI2CBusAtmelSAMD21::PIN_CONFIG_DESC pinConfig = {};
+        {
+            // TODO IMPLEMENT
+            pinConfig.pad0 = PINMUX_PA08D_SERCOM2_PAD0;
+            pinConfig.pad1 = PINMUX_PA09D_SERCOM2_PAD1;
+            pinConfig.pad2 = PINMUX_UNUSED;
+            pinConfig.pad3 = PINMUX_UNUSED;
+        }
+
+        CI2CBusAtmelSAMD21::DESC desc = {};
+        desc.id                       = 0;
+        desc.sercomI2c                = &(SERCOM2->I2CM);
+        desc.pin_config               = pinConfig;
+
+        // create I2C bus
+        // TODO: Remove dynamic memory allocation
+        m_i2c_bus = new CI2CBusAtmelSAMD21(desc, m_pins);
+    }
+
+    // configure I2C bus
+    {
+        CI2CBusAtmelSAMD21::CONFIG_DESC busConfig = {};
+        {
+            busConfig.enable_run_in_standby = true;
+            busConfig.sda_hold_time         = CI2CBusAtmelSAMD21::SDA_HOLD_TIME::NS_450;
+            busConfig.manager_baud          = 0xff; // TODO HACK
+            busConfig.enable_smart_mode     = true;
+            busConfig.transfer_speed        = CI2CBusAtmelSAMD21::TRANSFER_SPEED::STANDARD;
+        }
+
+        // set config
+        m_i2c_bus->SetConfig(busConfig);
+
+        // set role
+        // TODO HACK: This I2C bus only supports ROLE::MASTER
+        m_i2c_bus->SetRole(CI2CEntity::ROLE::MANAGER);
+
+        // enable I2C
+        m_i2c_bus->SetEnabled(true);
+    }
 }
 
 void CProgram::OnUpdate() {
-	m_i2c_bus->Start(0x12, CI2CBus::TRANSFER_DIRECTION::WRITE);
-	for(uint8_t i = 0; i < 8; i++){
-		m_i2c_bus->Write(i);
-	}
-	m_i2c_bus->Stop();
+    m_i2c_bus->Start(0x11, CI2CBus::TRANSFER_DIRECTION::WRITE);
+    for (uint8_t i = 0; i < 8; i++) {
+        m_i2c_bus->Write(i);
+    }
+    m_i2c_bus->Stop();
 }
-
--- test_samd21/i2c/program.h
+++ test_samd21/i2c/program.h
@@ -4,23 +4,22 @@
 #include <azydev/embedded/pins/atmel/samd21/pins.h>
 
 class CI2CBus;
+class CClock;
 
-class CProgram final {
-	public:
+class CProgram final
+{
+public:
+    CProgram();
+    virtual ~CProgram();
+    void Main();
 
-	CProgram();
-	virtual ~CProgram();
-	void Main();
+private:
+    // member variables
+    CPinsAtmelSAMD21 m_pins;
+    CClock* m_i2c_clock;
+    CI2CBus* m_i2c_bus;
 
-	private:
-	
-	// member variables
-	CLoggerAtmelSAMD21 m_logger;
-	CPinsAtmelSAMD21 m_pins;
-	CI2CBus* m_i2c_bus;
-
-	// methods
-	void OnInit();
-	void OnUpdate();
+    // methods
+    void OnInit();
+    void OnUpdate();
 };
-
