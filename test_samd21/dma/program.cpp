9ab9e5244b9c2edcae0635d518845f02bc5408b5
azydevelopment <29720335+azydevelopment@users.noreply.github.com>
2018-01-30 19:49:06 -0800

    Made DMA test source and destination data variables file scoped

--- test_samd21/dma/program.cpp
+++ test_samd21/dma/program.cpp
@@ -4,6 +4,12 @@
 #include <azydev/embedded/dma/atmel/samd21/engine.h>
 #include <cstring>
 
+/* FILE SCOPED STATICS */
+
+static const uint16_t NUM_BYTES   = 64;
+static uint8_t dataSrc[NUM_BYTES] = {};
+static uint8_t dataDst[NUM_BYTES] = {};
+
 /* PUBLIC */
 
 CProgram::CProgram()
@@ -21,10 +27,6 @@ void CProgram::Main() {
         OnUpdate();
     }
 }
-
-const uint16_t NUM_BYTES = 8;
-uint8_t dataSrc[NUM_BYTES] = {};
-uint8_t dataDst[NUM_BYTES] = {};
 
 void CProgram::OnInit() {
     // init system
@@ -51,24 +53,25 @@ void CProgram::OnInit() {
 
         // create DMA engine
         m_dma_engine = new CDMAEngineAtmelSAMD21(desc);
-		
-		// enable DMA engine
-		m_dma_engine->SetEnabled(true);
+
+        // enable DMA engine
+        m_dma_engine->SetEnabled(true);
     }
-	
-	for(uint16_t i = 0; i < NUM_BYTES; i++) {
-		dataSrc[i] = i;
-	}
+
+    // populate source data
+    for (uint16_t i = 0; i < NUM_BYTES; i++) {
+        dataSrc[i] = i;
+    }
 }
 
 void CProgram::OnUpdate() {
     // init DMA transfer
     CDMAChannelAtmelSAMD21::TRANSFER_DESC transferDesc = {};
     {
-		transferDesc.callback_transfer_complete = nullptr;
-        transferDesc.priority            = CDMAChannelAtmelSAMD21::PRIORITY::LVL_0;
-        transferDesc.trigger             = CDMAChannelAtmelSAMD21::TRIGGER::SOFTWARE_OR_EVENT;
-        transferDesc.trigger_action      = CDMAChannelAtmelSAMD21::TRIGGER_ACTION::START_TRANSACTION;
+        transferDesc.callback_transfer_complete = nullptr;
+        transferDesc.priority                   = CDMAChannelAtmelSAMD21::PRIORITY::LVL_0;
+        transferDesc.trigger        = CDMAChannelAtmelSAMD21::TRIGGER::SOFTWARE_OR_EVENT;
+        transferDesc.trigger_action = CDMAChannelAtmelSAMD21::TRIGGER_ACTION::START_TRANSACTION;
         transferDesc.enable_event_output = false;
         transferDesc.enable_event_input  = false;
         transferDesc.event_input_action  = CDMAChannelAtmelSAMD21::EVENT_INPUT_ACTION::NOACT;
@@ -82,19 +85,19 @@ void CProgram::OnUpdate() {
         transferDesc.step_size_select =
             CDMAChannelAtmelSAMD21::DESCRIPTOR::STEP_SIZE_SELECT::DESTINATION;
         transferDesc.step_size           = CDMAChannelAtmelSAMD21::DESCRIPTOR::STEP_SIZE::X1;
-        transferDesc.num_beats = NUM_BYTES;
+        transferDesc.num_beats           = NUM_BYTES;
         transferDesc.source_address      = reinterpret_cast<uint32_t>(dataSrc) + NUM_BYTES;
         transferDesc.destination_address = reinterpret_cast<uint32_t>(dataDst) + NUM_BYTES;
-        transferDesc.enable_interrupt_transfer_error    = true;
-        transferDesc.enable_interrupt_channel_suspend   = true;
+        transferDesc.enable_interrupt_transfer_error  = true;
+        transferDesc.enable_interrupt_channel_suspend = true;
     }
 
-	IDMAEntity::ITransferControl* const transferControl = nullptr;
-	m_dma_engine->AddTransfer(transferDesc, &transferControl);
-	
-	while(transferControl->IsTransferInProgress()) {
-		if(transferControl->IsPendingTrigger()) {
-			transferControl->TriggerTransferStep();
-		}
-	}
+    IDMAEntity::ITransferControl* transferControl = nullptr;
+    m_dma_engine->AddTransfer(transferDesc, &transferControl);
+
+    while (transferControl->IsTransferInProgress()) {
+        if (transferControl->IsPendingTrigger()) {
+            transferControl->TriggerTransferStep();
+        }
+    }
 }
